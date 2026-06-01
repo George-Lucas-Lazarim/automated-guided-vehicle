c128cb580b61936528e6b7213e03d929dda12640
George Lucas Lazarim <georgelucas1402@gmail.com>
2026-05-31 21:19:36 -0300

    create queue function

--- queue.c
+++ queue.c
@@ -1,1 +1,9 @@
 #include "queue.h"
+
+struct queue* createQueue() {
+    struct queue* newQueue = (struct queue*) malloc (sizeof(struct queue));
+    if (newQueue == NULL) return newQueue;
+
+    newQueue->front = newQueue->rear = NULL;
+    return newQueue;
+}
--- queue.h
+++ queue.h
@@ -1,6 +1,20 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
+#include <stdio.h>
+#include <stdlib.h>
 
+struct queueNode {
+    int orderID;
+    int productID;
+    struct queueNode* next;
+};
+
+struct queue {
+    struct queueNode front;
+    struct queueNode rear;  
+};
+
+struct queue* createQueue();
 
 #endif
