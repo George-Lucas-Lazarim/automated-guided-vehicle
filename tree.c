664038e2b22bda21d34f4d8ee9f59c5cb8df14a5
George Lucas Lazarim <georgelucas1402@gmail.com>
2026-06-03 14:41:24 -0300

    basic functions to tree

--- tree.c
+++ tree.c
@@ -1,1 +1,53 @@
 #include "tree.h"
+
+struct treeNode* initNode (int orderID) {
+    if (orderID < 0) {
+        printf("\nError! invalid order ID");
+        return NULL;
+    }
+
+    struct treeNode* newNode = (struct treeNode*) malloc (sizeof(struct treeNode));
+
+    if (newNode == NULL) {
+        printf("\nError! Memory allocation failed");
+        return NULL;
+    }
+
+    newNode->orderID = orderID;
+    newNode->left = newNode->right = NULL;
+
+    return newNode;
+}
+
+struct treeNode* addOrderID (struct treeNode* root, int orderID) {
+    struct treeNode* auxPointer = root;
+    struct treeNode* prevPointer = NULL;
+
+    while (auxPointer != NULL) {
+        prevPointer = auxPointer;
+        if (auxPointer->orderID > orderID) auxPointer = auxPointer->left;
+        else if (auxPointer->orderID <= orderID) auxPointer = auxPointer->right;
+    }
+
+    if (prevPointer == NULL) return initNode(orderID);
+
+    auxPointer = initNode(orderID);
+
+    if (prevPointer->orderID > orderID) prevPointer->left = auxPointer;
+    else prevPointer->right = auxPointer;
+
+    return root;
+}
+
+bool searchOrderID (struct treeNode* root, int orderID) {
+    struct treeNode* auxPointer = root;
+
+    while (auxPointer != NULL) {
+        if (auxPointer->orderID == orderID) return true;
+
+        if (auxPointer->orderID > orderID) auxPointer = auxPointer->left;
+        else if (auxPointer->orderID <= orderID) auxPointer = auxPointer->right;
+    }
+
+    return false;
+}
--- tree.h
+++ tree.h
@@ -1,6 +1,18 @@
 #ifndef TREE_H
 #define TREE_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
+struct treeNode {
+    int orderID;
+    struct treeNode* left;
+    struct treeNode* right;
+};
+
+struct treeNode* initNode (int orderID);
+struct treeNode* addOrderID (struct treeNode* root, int orderID);
+bool searchOrderID (struct treeNode* root, int orderID);
 
 #endif
